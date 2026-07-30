f0329b932d3254fa6d78efd354fe8f1f46c11165
Artur Santos <arturcastrodossantos.com@gmail.com>
2026-07-29 21:04:11 -0300

    fix: valores padrões nas tfvars

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,9 +1,11 @@
 variable "region" {
   type        = string
   description = "Região da AWS"
+  default     = "us-east-2"
 }
 
 variable "profile" {
   type        = string
   description = "Profile da AWS"
+  default     = null
 }
